8a86f5dc36e7855398d3e008809aa012249ca49b
rgarro <rgarro@gmail.com>
2017-09-02 21:22:22 -0600

    isla chira subterranea independiente terreno internacional ...

--- Assets/_Completed-Assets/Scripts/Tank/TankHealth.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankHealth.cs
@@ -1,91 +1,116 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-namespace Complete
+public class TankHealth : NetworkBehaviour
 {
-    public class TankHealth : MonoBehaviour
+    public float m_StartingHealth = 100f;             // The amount of health each tank starts with.
+    public Slider m_Slider;                           // The slider to represent how much health the tank currently has.
+    public Image m_FillImage;                         // The image component of the slider.
+    public Color m_FullHealthColor = Color.green;     // The color the health bar will be when on full health.
+    public Color m_ZeroHealthColor = Color.red;       // The color the health bar will be when on no health.
+    public AudioClip m_TankExplosion;                 // The clip to play when the tank explodes.
+    public ParticleSystem m_ExplosionParticles;       // The particle system the will play when the tank is destroyed.
+    public GameObject m_TankRenderers;                // References to all the gameobjects that need to be disabled when the tank is dead.
+    public GameObject m_HealthCanvas;
+    public GameObject m_AimCanvas;
+    public GameObject m_LeftDustTrail;
+    public GameObject m_RightDustTrail;
+    public TankSetup m_Setup;
+    public TankManager m_Manager;                   //Associated manager, to disable control when dying.
+
+    [SyncVar(hook = "OnCurrentHealthChanged")]
+    private float m_CurrentHealth;                  // How much health the tank currently has.*
+    [SyncVar]
+    private bool m_ZeroHealthHappened;              // Has the tank been reduced beyond zero health yet?
+    private BoxCollider m_Collider;                 // Used so that the tank doesn't collide with anything when it's dead.
+
+
+    private void Awake()
     {
-        public float m_StartingHealth = 100f;               // The amount of health each tank starts with.
-        public Slider m_Slider;                             // The slider to represent how much health the tank currently has.
-        public Image m_FillImage;                           // The image component of the slider.
-        public Color m_FullHealthColor = Color.green;       // The color the health bar will be when on full health.
-        public Color m_ZeroHealthColor = Color.red;         // The color the health bar will be when on no health.
-        public GameObject m_ExplosionPrefab;                // A prefab that will be instantiated in Awake, then used whenever the tank dies.
-        
-        
-        private AudioSource m_ExplosionAudio;               // The audio source to play when the tank explodes.
-        private ParticleSystem m_ExplosionParticles;        // The particle system the will play when the tank is destroyed.
-        private float m_CurrentHealth;                      // How much health the tank currently has.
-        private bool m_Dead;                                // Has the tank been reduced beyond zero health yet?
+        m_Collider = GetComponent<BoxCollider>();
+    }
 
 
-        private void Awake ()
+    // This is called whenever the tank takes damage.
+    public void Damage(float amount)
+    {
+        // Reduce current health by the amount of damage done.
+        m_CurrentHealth -= amount;
+
+        // If the current health is at or below zero and it has not yet been registered, call OnZeroHealth.
+        if (m_CurrentHealth <= 0f && !m_ZeroHealthHappened)
         {
-            // Instantiate the explosion prefab and get a reference to the particle system on it.
-            m_ExplosionParticles = Instantiate (m_ExplosionPrefab).GetComponent<ParticleSystem> ();
-
-            // Get a reference to the audio source on the instantiated prefab.
-            m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource> ();
-
-            // Disable the prefab so it can be activated when it's required.
-            m_ExplosionParticles.gameObject.SetActive (false);
+            OnZeroHealth();
         }
+    }
 
 
-        private void OnEnable()
-        {
-            // When the tank is enabled, reset the tank's health and whether or not it's dead.
-            m_CurrentHealth = m_StartingHealth;
-            m_Dead = false;
+    private void SetHealthUI()
+    {
+        // Set the slider's value appropriately.
+        m_Slider.value = m_CurrentHealth;
 
-            // Update the health slider's value and color.
-            SetHealthUI();
-        }
-
-
-        public void TakeDamage (float amount)
-        {
-            // Reduce current health by the amount of damage done.
-            m_CurrentHealth -= amount;
-
-            // Change the UI elements appropriately.
-            SetHealthUI ();
-
-            // If the current health is at or below zero and it has not yet been registered, call OnDeath.
-            if (m_CurrentHealth <= 0f && !m_Dead)
-            {
-                OnDeath ();
-            }
-        }
+        // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+    }
 
 
-        private void SetHealthUI ()
-        {
-            // Set the slider's value appropriately.
-            m_Slider.value = m_CurrentHealth;
+    void OnCurrentHealthChanged(float value)
+    {
+        m_CurrentHealth = value;
+        // Change the UI elements appropriately.
+        SetHealthUI();
 
-            // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
-            m_FillImage.color = Color.Lerp (m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
-        }
+    }
 
+    private void OnZeroHealth()
+    {
+        // Set the flag so that this function is only called once.
+        m_ZeroHealthHappened = true;
 
-        private void OnDeath ()
-        {
-            // Set the flag so that this function is only called once.
-            m_Dead = true;
+        RpcOnZeroHealth();
+    }
 
-            // Move the instantiated explosion prefab to the tank's position and turn it on.
-            m_ExplosionParticles.transform.position = transform.position;
-            m_ExplosionParticles.gameObject.SetActive (true);
+    private void InternalOnZeroHealth()
+    {
+        // Disable the collider and all the appropriate child gameobjects so the tank doesn't interact or show up when it's dead.
+        SetTankActive(false);
+    }
 
-            // Play the particle system of the tank exploding.
-            m_ExplosionParticles.Play ();
+    [ClientRpc]
+    private void RpcOnZeroHealth()
+    {
+        // Play the particle system of the tank exploding.
+        m_ExplosionParticles.Play();
 
-            // Play the tank explosion sound effect.
-            m_ExplosionAudio.Play();
+        // Create a gameobject that will play the tank explosion sound effect and then destroy itself.
+        AudioSource.PlayClipAtPoint(m_TankExplosion, transform.position);
 
-            // Turn the tank off.
-            gameObject.SetActive (false);
-        }
+        InternalOnZeroHealth();
+    }
+
+    private void SetTankActive(bool active)
+    {
+        m_Collider.enabled = active;
+
+        m_TankRenderers.SetActive(active);
+        m_HealthCanvas.SetActive(active);
+        m_AimCanvas.SetActive(active);
+        m_LeftDustTrail.SetActive(active);
+        m_RightDustTrail.SetActive(active);
+
+        if (active) m_Manager.EnableControl();
+        else m_Manager.DisableControl();
+
+        m_Setup.ActivateCrown(active);
+    }
+
+    // This function is called at the start of each round to make sure each tank is set up correctly.
+    public void SetDefaults()
+    {
+        m_CurrentHealth = m_StartingHealth;
+        m_ZeroHealthHappened = false;
+        SetTankActive(true);
     }
 }
--- Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
@@ -1,141 +1,150 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-namespace Complete
+public class TankMovement : NetworkBehaviour
 {
-    public class TankMovement : MonoBehaviour
+    public int m_PlayerNumber = 1;                // Used to identify which tank belongs to which player.  This is set by this tank's manager.
+    public int m_LocalID = 1;
+    public float m_Speed = 12f;                   // How fast the tank moves forward and back.
+    public float m_TurnSpeed = 180f;              // How fast the tank turns in degrees per second.
+    public float m_PitchRange = 0.2f;             // The amount by which the pitch of the engine noises can vary.
+    public AudioSource m_MovementAudio;           // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
+    public AudioClip m_EngineIdling;              // Audio to play when the tank isn't moving.
+    public AudioClip m_EngineDriving;             // Audio to play when the tank is moving.
+    public ParticleSystem m_LeftDustTrail;        // The particle system of dust that is kicked up from the left track.
+    public ParticleSystem m_RightDustTrail;       // The particle system of dust that is kicked up from the rightt track.
+    public Rigidbody m_Rigidbody;              // Reference used to move the tank.
+
+    private string m_MovementAxis;              // The name of the input axis for moving forward and back.
+    private string m_TurnAxis;                  // The name of the input axis for turning.
+    private float m_MovementInput;              // The current value of the movement input.
+    private float m_TurnInput;                  // The current value of the turn input.
+    private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
+
+
+    private void Awake()
     {
-        public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
-        public float m_Speed = 12f;                 // How fast the tank moves forward and back.
-        public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
-        public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-        public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
-        public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-		public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
 
-        private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
-        private string m_TurnAxisName;              // The name of the input axis for turning.
-        private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-        private float m_MovementInputValue;         // The current value of the movement input.
-        private float m_TurnInputValue;             // The current value of the turn input.
-        private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
-        private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
 
-        private void Awake ()
+    private void Start()
+    {
+        // The axes are based on player number.
+        m_MovementAxis = "Vertical" + (m_LocalID + 1);
+        m_TurnAxis = "Horizontal" + (m_LocalID + 1);
+
+        // Store the original pitch of the audio source.
+        m_OriginalPitch = m_MovementAudio.pitch;
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        // Store the value of both input axes.
+        m_MovementInput = Input.GetAxis(m_MovementAxis);
+        m_TurnInput = Input.GetAxis(m_TurnAxis);
+
+        EngineAudio();
+    }
+
+
+    private void EngineAudio()
+    {
+        // If there is no input (the tank is stationary)...
+        if (Mathf.Abs(m_MovementInput) < 0.1f && Mathf.Abs(m_TurnInput) < 0.1f)
         {
-            m_Rigidbody = GetComponent<Rigidbody> ();
-        }
-
-
-        private void OnEnable ()
-        {
-            // When the tank is turned on, make sure it's not kinematic.
-            m_Rigidbody.isKinematic = false;
-
-            // Also reset the input values.
-            m_MovementInputValue = 0f;
-            m_TurnInputValue = 0f;
-
-            // We grab all the Particle systems child of that Tank to be able to Stop/Play them on Deactivate/Activate
-            // It is needed because we move the Tank when spawning it, and if the Particle System is playing while we do that
-            // it "think" it move from (0,0,0) to the spawn point, creating a huge trail of smoke
-            m_particleSystems = GetComponentsInChildren<ParticleSystem>();
-            for (int i = 0; i < m_particleSystems.Length; ++i)
+            // ... and if the audio source is currently playing the driving clip...
+            if (m_MovementAudio.clip == m_EngineDriving)
             {
-                m_particleSystems[i].Play();
+                // ... change the clip to idling and play it.
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
             }
         }
-
-
-        private void OnDisable ()
+        else
         {
-            // When the tank is turned off, set it to kinematic so it stops moving.
-            m_Rigidbody.isKinematic = true;
-
-            // Stop all particle system so it "reset" it's position to the actual one instead of thinking we moved when spawning
-            for(int i = 0; i < m_particleSystems.Length; ++i)
+            // Otherwise if the tank is moving and the idling clip is currently playing...
+            if (m_MovementAudio.clip == m_EngineIdling)
             {
-                m_particleSystems[i].Stop();
+                // ... change the clip to driving and playing.
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
             }
         }
+    }
 
 
-        private void Start ()
-        {
-            // The axes names are based on player number.
-            m_MovementAxisName = "Vertical" + m_PlayerNumber;
-            m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+            return;
 
-            // Store the original pitch of the audio source.
-            m_OriginalPitch = m_MovementAudio.pitch;
-        }
-
-
-        private void Update ()
-        {
-            // Store the value of both input axes.
-            m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
-            m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
-
-            EngineAudio ();
-        }
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
+        Move();
+        Turn();
+    }
 
 
-        private void EngineAudio ()
-        {
-            // If there is no input (the tank is stationary)...
-            if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
-            {
-                // ... and if the audio source is currently playing the driving clip...
-                if (m_MovementAudio.clip == m_EngineDriving)
-                {
-                    // ... change the clip to idling and play it.
-                    m_MovementAudio.clip = m_EngineIdling;
-                    m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play ();
-                }
-            }
-            else
-            {
-                // Otherwise if the tank is moving and if the idling clip is currently playing...
-                if (m_MovementAudio.clip == m_EngineIdling)
-                {
-                    // ... change the clip to driving and play.
-                    m_MovementAudio.clip = m_EngineDriving;
-                    m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play();
-                }
-            }
-        }
+    private void Move()
+    {
+        // Create a movement vector based on the input, speed and the time between frames, in the direction the tank is facing.
+        Vector3 movement = transform.forward * m_MovementInput * m_Speed * Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position.
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
 
 
-        private void FixedUpdate ()
-        {
-            // Adjust the rigidbodies position and orientation in FixedUpdate.
-            Move ();
-            Turn ();
-        }
+    private void Turn()
+    {
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = m_TurnInput * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion inputRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * inputRotation);
+    }
 
 
-        private void Move ()
-        {
-            // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-            Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+    // This function is called at the start of each round to make sure each tank is set up correctly.
+    public void SetDefaults()
+    {
+        m_Rigidbody.velocity = Vector3.zero;
+        m_Rigidbody.angularVelocity = Vector3.zero;
 
-            // Apply this movement to the rigidbody's position.
-            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-        }
+        m_MovementInput = 0f;
+        m_TurnInput = 0f;
 
+        m_LeftDustTrail.Clear();
+        m_LeftDustTrail.Stop();
 
-        private void Turn ()
-        {
-            // Determine the number of degrees to be turned based on the input, speed and time between frames.
-            float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+        m_RightDustTrail.Clear();
+        m_RightDustTrail.Stop();
+    }
 
-            // Make this into a rotation in the y axis.
-            Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+    public void ReEnableParticles()
+    {
+        m_LeftDustTrail.Play();
+        m_RightDustTrail.Play();
+    }
 
-            // Apply this rotation to the rigidbody's rotation.
-            m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
-        }
+    //We freeze the rigibody when the control is disabled to avoid the tank drifting!
+    protected RigidbodyConstraints m_OriginalConstrains;
+    void OnDisable()
+    {
+        m_OriginalConstrains = m_Rigidbody.constraints;
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    void OnEnable()
+    {
+        m_Rigidbody.constraints = m_OriginalConstrains;
     }
 }
