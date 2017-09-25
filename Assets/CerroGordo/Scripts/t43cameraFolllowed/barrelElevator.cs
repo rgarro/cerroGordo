dfc7b4072f1c962535780315ff77ef7a9d294882
rgarro <rgarro@gmail.com>
2017-09-25 16:18:29 -0600

    [#151358673] baikal lake factory report

---
+++ Assets/CerroGordo/Scripts/t43cameraFolllowed/barrelElevator.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * turns vertically rotationSteps current transform
+ * clockwise or counterclockwise from input turn key
+ * 
+ * @rolando <rolando@emptyart.xyz>
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrelElevator : MonoBehaviour {
+
+	public string upTurnKey;
+	public string downTurnKey;
+	public float rotationSteps;
+	public float yPivot;
+	public float zPivot;
+	private float xRot;
+	private Vector3 tPos;
+
+
+	void Start () {
+		//this.xRot = this.transform.rotation.x;
+		this.xRot = 0;
+	}
+
+
+	void Update () {
+		//Debug.Log ("here IM is only rocknroll ....");
+		if(Input.GetKeyDown(this.downTurnKey)){
+			this.xRot = this.xRot - rotationSteps;
+			Quaternion target = Quaternion.Euler(this.xRot,this.yPivot,this.zPivot);
+			this.transform.SetPositionAndRotation(this.transform.position,target);
+		}
+		if(Input.GetKeyDown(this.upTurnKey)){
+			this.xRot = this.xRot + rotationSteps;
+			Quaternion target = Quaternion.Euler(this.xRot,this.yPivot,this.zPivot);
+			this.transform.SetPositionAndRotation(this.transform.position,target);
+		}
+	}
+}
--- Assets/CerroGordo/Scripts/t43cameraFolllowed/turretRoller.cs
+++ Assets/CerroGordo/Scripts/t43cameraFolllowed/turretRoller.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/**
+ * turns horizontally rotationSteps current transform
+ * clockwise or counterclockwise from input turn key
+ * 
+ * @rolando <rolando@emptyart.xyz>
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
