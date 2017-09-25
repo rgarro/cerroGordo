fb387036c705586481576b5a41489ea1c457d6e1
rgarro <rgarro@gmail.com>
2017-09-24 20:07:48 -0600

    [Delivers #151237311]fucking turret turns otto carius is dead i want a new car ...

---
+++ Assets/CerroGordo/Scripts/t43cameraFolllowed/turretRoller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretRoller : MonoBehaviour {
+
+	public string leftTurnKey;
+	public string rightTurnKey;
+	public int rotationSteps;
+	public float xPivot;
+	public float zPivot;
+	private float yRot;
+	private Vector3 tPos;
+
+
+	void Start () {
+		this.yRot = this.transform.rotation.y;
+	}
+	
+
+	void Update () {
+		//Debug.Log ("here IM is only rocknroll ....");
+		if(Input.GetKeyDown(this.leftTurnKey)){
+			this.yRot = this.yRot - rotationSteps;
+			Quaternion target = Quaternion.Euler(this.xPivot,this.yRot,this.zPivot);
+			this.transform.SetPositionAndRotation(this.transform.position,target);
+		}
+		if(Input.GetKeyDown(this.rightTurnKey)){
+			this.yRot = this.yRot + rotationSteps;
+			Quaternion target = Quaternion.Euler(this.xPivot,this.yRot,this.zPivot);
+			this.transform.SetPositionAndRotation(this.transform.position,target);
+		}
+	}
+}
