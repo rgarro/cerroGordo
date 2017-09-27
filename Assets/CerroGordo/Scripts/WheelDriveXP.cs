e77a83e3f6562713a44f3a9db1377ceeef6c911c
rgarro <rgarro@gmail.com>
2017-09-27 13:38:54 -0600

    [Delivers #151463932] mi general noriega no va a salir ..

--- Assets/CerroGordo/Scripts/WheelDriveXP.cs
+++ Assets/CerroGordo/Scripts/WheelDriveXP.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 using System;
 
-[Serializable]
+/*[Serializable]
 public enum DriveType
 {
 	RearWheelDrive,
 	FrontWheelDrive,
 	AllWheelDrive
-}
+}*/
 
-public class WheelDrive : MonoBehaviour
+public class WheelDriveXP : MonoBehaviour
 {
-    [Tooltip("Maximum steering angle of the wheels")]
+	[Tooltip("Maximum steering angle of the wheels")]
 	public float maxAngle = 30f;
 	[Tooltip("Maximum torque applied to the driving wheels")]
 	public float maxTorque = 300f;
@@ -30,29 +30,17 @@ public class WheelDrive : MonoBehaviour
 	[Tooltip("The vehicle's drive type: rear-wheels drive, front-wheels drive or all-wheels drive.")]
 	public DriveType driveType;
 
-    private WheelCollider[] m_Wheels;
+	private WheelCollider[] m_Wheels;
 
-    // Find all the WheelColliders down in the hierarchy.
+	public Transform visualCar;
+
+	// Find all the WheelColliders down in the hierarchy.
 	void Start()
 	{
 		m_Wheels = GetComponentsInChildren<WheelCollider>();
 
-		for (int i = 0; i < m_Wheels.Length; ++i) 
-		{
-			var wheel = m_Wheels [i];
-
-			// Create wheel shapes only when needed.
-			if (wheelShape != null)
-			{
-				var ws = Instantiate (wheelShape);
-				ws.transform.parent = wheel.transform;
-			}
-		}
 	}
 
-	// This is a really simple approach to updating wheels.
-	// We simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero.
-	// This helps us to figure our which wheels are front ones and which are rear.
 	void Update()
 	{
 		m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
@@ -84,14 +72,13 @@ public class WheelDrive : MonoBehaviour
 			}
 
 			// Update visual wheels if any.
-			if (wheelShape) 
 			{
 				Quaternion q;
 				Vector3 p;
 				wheel.GetWorldPose (out p, out q);
 
 				// Assume that the only child of the wheelcollider is the wheel shape.
-				Transform shapeTransform = wheel.transform.GetChild (0);
+				Transform shapeTransform = visualCar.Find(wheel.name);//wheel.transform.GetChild (0);
 				shapeTransform.position = p;
 				shapeTransform.rotation = q;
 			}
