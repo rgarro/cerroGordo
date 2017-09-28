c7b35fff92351e8f293b3b2aba1bb7061678d9f6
rgarro <rgarro@gmail.com>
2017-09-28 12:15:45 -0600

    [#151357687] cada dia el capitalismo se hace mas fuerte

--- Assets/CerroGordo/Scripts/t43cameraFolllowed/barrelElevator.cs
+++ Assets/CerroGordo/Scripts/t43cameraFolllowed/barrelElevator.cs
@@ -13,15 +13,24 @@ public class barrelElevator : MonoBehaviour {
 	public string upTurnKey;
 	public string downTurnKey;
 	public float rotationSteps;
+	private AudioSource servoSound;
+
+	void Start () {
+		this.servoSound = GetComponent<AudioSource> ();
+	}
 
 	void Update () {
 		//Debug.Log ("here IM is only rocknroll ....");
-
 		if(Input.GetKeyDown(this.downTurnKey)){
+			this.servoSound.Play ();
 			transform.Rotate(Vector3.right * Time.deltaTime * rotationSteps);
 		}
 		if(Input.GetKeyDown(this.upTurnKey)){
+			this.servoSound.Play ();
 			transform.Rotate(Vector3.left * Time.deltaTime * rotationSteps);
+		}
+		if (Input.GetKeyUp (this.downTurnKey) || Input.GetKeyUp(this.upTurnKey)) {
+			this.servoSound.Stop ();
 		}
 	}
 }
--- Assets/CerroGordo/Scripts/t43cameraFolllowed/turretRoller.cs
+++ Assets/CerroGordo/Scripts/t43cameraFolllowed/turretRoller.cs
@@ -17,26 +17,32 @@ public class turretRoller : MonoBehaviour {
 	public float zPivot;
 	private float yRot;
 	private Vector3 tPos;
-
+	private AudioSource servoSound;
 
 	void Start () {
 		this.yRot = this.transform.rotation.y;
+		this.servoSound = GetComponent<AudioSource> ();
 	}
 	//ojo la turreta viene torcida de ukrania para torcer los cables de detonar la municion
 
 	void Update () {
 		//Debug.Log ("here IM is only rocknroll ....");
 		if(Input.GetKeyDown(this.leftTurnKey)){
+			this.servoSound.Play ();
 			this.yRot = this.yRot - rotationSteps;
 			Quaternion target = Quaternion.Euler(this.xPivot,this.yRot,this.zPivot);
 			this.transform.SetPositionAndRotation(this.transform.position,target);
 			//transform.Rotate(Vector3.left * Time.deltaTime * rotationSteps);///la 
 		}
 		if(Input.GetKeyDown(this.rightTurnKey)){
+			this.servoSound.Play ();
 			//transform.Rotate(Vector3.right * Time.deltaTime * rotationSteps);
 			this.yRot = this.yRot + rotationSteps;
 			Quaternion target = Quaternion.Euler(this.xPivot,this.yRot,this.zPivot);
 			this.transform.SetPositionAndRotation(this.transform.position,target);
+		}
+		if (Input.GetKeyUp (this.rightTurnKey) || Input.GetKeyUp(this.leftTurnKey)) {
+			this.servoSound.Stop ();
 		}
 	}
 }
