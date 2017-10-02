ef262815480f9aff24103b38a06ec5bf635fc7c3
rgarro <rgarro@gmail.com>
2017-10-02 15:31:57 -0600

    [Delivers #151618523] aqui yo soy el capitan pinga loca ...

--- Assets/CerroGordo/Scripts/sphereTurret/turretController.cs
+++ Assets/CerroGordo/Scripts/sphereTurret/turretController.cs
@@ -16,14 +16,61 @@ public class turretController : MonoBehaviour {
 	private bool gUP;
 	private bool gDown;
 	public float elevationSteps;
+	public string leftTurnKey;
+	public string rightTurnKey;
+	private bool gRight;
+	private bool gLeft;
+	private AudioSource servoSound;
+	public bool play_servo = false;
 
 	void Start () {
+		this.servoSound = GetComponent<AudioSource> ();
 		this.gUP = false;
 		this.gDown = false;
+		this.gRight = false;
+		this.gLeft = false;
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		
+		//Debug.Log ("here IM is only rocknroll ....");
+		if(Input.GetKeyDown(this.rightTurnKey)){
+			if (play_servo) {
+				if (!this.servoSound.isPlaying) {
+					this.servoSound.Play ();
+				}
+			}
+			this.gRight = true;
+		}
+		if(this.gRight){
+			transform.Rotate(Vector3.up  * rotationSteps);
+		}
+		if(Input.GetKeyDown(this.leftTurnKey)){
+			this.gLeft = true;
+		}
+		if(this.gLeft){
+			if (play_servo) {
+				if (!this.servoSound.isPlaying) {
+					this.servoSound.Play ();
+				}
+			}
+
+			transform.Rotate(Vector3.down  * rotationSteps);
+		}
+		if (Input.GetKeyUp (this.leftTurnKey) || Input.GetKeyUp(this.rightTurnKey)) {
+			if (play_servo) {
+				this.servoSound.Stop ();
+			}
+			this.gLeft = false;
+			this.gRight = false;
+
+		}
+		if (Input.GetKeyUp (this.upTurnKey) || Input.GetKeyUp(this.downTurnKey)) {
+			this.gUP = false;
+			this.gDown = false;
+			if (play_servo) {
+				this.servoSound.Stop ();
+			}
+		}
 	}
 }
