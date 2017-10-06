e16b5f060167a2b03599c8e911d4533aa26e49c5
rgarro <rgarro@gmail.com>
2017-10-06 13:48:48 -0600

    [#151540656] Me sirve a mi o va pal carajo Oye revolucion compadre

--- Assets/CerroGordo/Scripts/t43cameraFolllowed/barrelElevator.cs
+++ Assets/CerroGordo/Scripts/t43cameraFolllowed/barrelElevator.cs
@@ -17,6 +17,9 @@ public class barrelElevator : MonoBehaviour {
 	private bool gUP;
 	private bool gDown;
 
+	public float minElev;
+	public float maxElev;
+
 	void Start () {
 		this.servoSound = GetComponent<AudioSource> ();
 		this.gUP = false;
@@ -24,29 +27,46 @@ public class barrelElevator : MonoBehaviour {
 	}
 
 	void Update () {
-		//Debug.Log ("here IM is only rocknroll ....");
+		Debug.Log (this.transform.localEulerAngles.x);
 		if(Input.GetKeyDown(this.downTurnKey)){
 			this.gDown = true;
 		}
 		if(this.gDown){
-			if (!this.servoSound.isPlaying) {
-				this.servoSound.Play ();
-			}
-			transform.Rotate(Vector3.right * Time.deltaTime * rotationSteps);
+			//if (this.transform.localEulerAngles.x > this.minElev) 
+				this.soundOn ();
+				this.rotateUp ();
+			
 		}
 		if(Input.GetKeyDown(this.upTurnKey)){
 			this.gUP = true;
 		}
 		if(this.gUP){
-			if (!this.servoSound.isPlaying) {
-				this.servoSound.Play ();
-			}
-			transform.Rotate(Vector3.left * Time.deltaTime * rotationSteps);
+			//if (this.transform.localEulerAngles.x < this.maxElev)
+				this.soundOn ();
+				this.rotateDown ();
 		}
 		if (Input.GetKeyUp (this.downTurnKey) || Input.GetKeyUp(this.upTurnKey)) {
 			this.servoSound.Stop ();
-			this.gUP = false;
-			this.gDown = false;
+			this.resetLed ();
 		}
+	}
+
+	public void rotateUp(){
+		transform.Rotate (Vector3.right * Time.deltaTime * rotationSteps);	
+	}
+
+	public void rotateDown(){
+		transform.Rotate (Vector3.left * Time.deltaTime * rotationSteps);
+	}
+
+	private void soundOn(){
+		if (!this.servoSound.isPlaying) {
+			this.servoSound.Play ();
+		}
+	}
+
+	private void resetLed(){
+		this.gUP = false;
+		this.gDown = false;
 	}
 }
