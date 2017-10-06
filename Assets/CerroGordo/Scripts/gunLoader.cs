b6a34ef714730f5e7cb7b670a96c5c21f83f70ce
rgarro <rgarro@gmail.com>
2017-10-05 18:39:31 -0600

    [Delivers #151358676] nikita krushev era un gallego comunista ...

--- Assets/CerroGordo/Scripts/gunLoader.cs
+++ Assets/CerroGordo/Scripts/gunLoader.cs
@@ -19,6 +19,8 @@ public class gunLoader : MonoBehaviour {
 	public float shootForce;
 	private float shootRateTime = 0f;
 	public GameObject turret;
+	public float bulletSpeed;
+	public float modelCorrectionAngle;
 
 
 	void Start () {
@@ -27,21 +29,13 @@ public class gunLoader : MonoBehaviour {
 	}
 
 	void Update () {
-		//Debug.Log ("rot X" + Mathf.Sin(this.transform.rotation.eulerAngles.x));
-		//Debug.Log ("loc X" + (this.transform.localEulerAngles.x - 360));
-		float rotX = this.transform.localEulerAngles.x - 360;
-		float x = this.transform.position.x * moveForce;
-		float y = this.transform.position.y * moveForce;
-		float z = this.transform.position.z * moveForce;
-		//Quaternion rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y * Time.deltaTime, turret.transform.rotation.z* Time.deltaTime);
-		Quaternion rotation = Quaternion.Euler(rotX,turret.transform.eulerAngles.y , this.transform.localEulerAngles.z );
-		Debug.Log (rotation);
-		Vector3 rbg = new Vector3(x,y,z);
-		Vector3 position = new Vector3(this.transform.position.x,this.transform.position.y + 2,this.transform.position.z);
+		float rotX = this.transform.localEulerAngles.x + modelCorrectionAngle ;
+		Quaternion rotation = Quaternion.Euler(rotX,turret.transform.eulerAngles.y,this.transform.localEulerAngles.z);
+		Vector3 position = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
 		if(Input.GetKey(KeyCode.Space)){
 			GameObject go = (GameObject)Instantiate (bullet,position,rotation);
-			//Rigidbody rb =  go.GetComponent<Rigidbody>();
-			//rb.AddForce (rbg);
+			Rigidbody rb =  go.GetComponent<Rigidbody>();
+			rb.velocity = transform.forward * bulletSpeed;
 		}
 	}
 }
