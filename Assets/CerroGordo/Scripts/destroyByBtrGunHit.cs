417ccf4e68e2c55d6eabb55c338d73b76d127d81
rgarro <rgarro@gmail.com>
2017-10-17 11:02:40 -0600

    [#151810603] Bullets fly but they don't give a fuck about who dies ...

---
+++ Assets/CerroGordo/Scripts/destroyByBtrGunHit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyByBtrGunHit : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		//Debug.Log ("tome chichiii...");
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
