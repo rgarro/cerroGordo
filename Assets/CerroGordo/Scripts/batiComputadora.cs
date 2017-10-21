667ef4700e6f5f32f4626b6172438c6f6e61e845
rgarro <rgarro@gmail.com>
2017-10-21 15:51:14 -0600

    [Delivers #151810644] a todos los maricones del norte los voy a matar yo ..

---
+++ Assets/CerroGordo/Scripts/batiComputadora.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batiComputadora : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	IEnumerator spawnWaves(){
+		yield return new WaitForSeconds (startWait);
+		while(true){
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(spawnWaves ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
