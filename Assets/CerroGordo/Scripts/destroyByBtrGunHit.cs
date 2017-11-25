75e72ca33c5290a7e6ee626d04a741658a4d3cd6
rgarro <rgarro@gmail.com>
2017-11-24 20:03:01 -0600

    [Delivers #152044435] 白犬

--- Assets/CerroGordo/Scripts/destroyByBtrGunHit.cs
+++ Assets/CerroGordo/Scripts/destroyByBtrGunHit.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class destroyByBtrGunHit : MonoBehaviour {
 
 	public GameObject Blast;
+	public int scoreValue;
 
 	void OnTriggerEnter(Collider other) {
 		//Debug.Log ("tome chichiii...");
 		if (other.tag == "Boundary") {
 			return;
 		}
+		GameObject bt = GameObject.FindGameObjectWithTag ("batiComputadora");
+		//Debug.Log (bt);
+		batiComputadora mu = bt.GetComponent<batiComputadora> ();
+		mu.addScore(scoreValue);
 		Instantiate (Blast,this.transform.position,this.transform.rotation);
 		Destroy(other.gameObject);
 		Destroy(gameObject);
