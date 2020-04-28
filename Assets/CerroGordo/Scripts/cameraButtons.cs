fee0cbd7f6b069567b5ac5579f8d273e5e0a1e67
rgarro <rgarro@gmail.com>
2020-04-27 22:50:18 -0600

    Maradona died on Falklands War ...

--- Assets/CerroGordo/Scripts/cameraButtons.cs
+++ Assets/CerroGordo/Scripts/cameraButtons.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 public class cameraButtons : MonoBehaviour
 {
     protected bool gun_camera_is_hidden =  false;
+    protected bool radar_camera_is_hidden =  false;
     protected int buttons_x_corner = 200;
 
     public GUISkin btnSkin;
-
     public Texture2D GunCamera2d;
     public Texture2D RadarIcon;
+    public GameObject gun_camera;
+    public GameObject radar_camera;
 
     void OnGUI(){
       GUI.skin = this.btnSkin;
       GUI.Box(new Rect(this.buttons_x_corner,10,100,90), "Cameras");
       if(GUI.Button(new Rect(this.buttons_x_corner+10,40,40,40), GunCamera2d))
         {
-            Debug.Log("add component here 111");
+            if(this.gun_camera_is_hidden){
+                this.gun_camera_is_hidden = false;
+                this.gun_camera.SetActive(true);
+            } else {
+                this.gun_camera_is_hidden = true;
+                this.gun_camera.SetActive(false);
+            }
         }
     
         if(GUI.Button(new Rect(this.buttons_x_corner+55,40,40,40), RadarIcon)) 
         {
-            Debug.Log("add component here 222");
+            if(this.radar_camera_is_hidden){
+                this.radar_camera_is_hidden = false;
+                this.radar_camera.SetActive(true);
+            } else {
+                this.radar_camera_is_hidden = true;
+                this.radar_camera.SetActive(false);
+            }
         }	
   	}
 
