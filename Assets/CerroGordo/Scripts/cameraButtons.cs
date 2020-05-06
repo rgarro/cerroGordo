67a6522688eeb0187a2d396ac9b482feb1f94f9e
rgarro <rgarro@gmail.com>
2020-05-05 20:36:18 -0600

    I guess it was the beatings made me wise ...

--- Assets/CerroGordo/Scripts/cameraButtons.cs
+++ Assets/CerroGordo/Scripts/cameraButtons.cs
@@ -20,26 +20,29 @@ public class cameraButtons : MonoBehaviour
 {
     protected bool gun_camera_is_hidden =  true;
     protected bool radar_camera_is_hidden =  true;
-    
+    protected bool rearview_camera_is_hidden = true;
     protected bool main_camera_is_hidden = false;
     protected int buttons_x_corner = 200;
 
     public GUISkin btnSkin;
     public Texture2D GunCamera2d;
     public Texture2D RadarIcon;
+    public Texture2D RearViewIcon;
     public GameObject gun_camera;
     public GameObject radar_camera;
     public GameObject main_camera;
+    public GameObject rearview_camera;
 
     void Start()
     {
         this.gun_camera.SetActive(false);
         this.radar_camera.SetActive(false);
+        this.rearview_camera.SetActive(false);
     }
 
     void OnGUI(){
       GUI.skin = this.btnSkin;
-      GUI.Box(new Rect(this.buttons_x_corner,10,100,90), "Cameras");
+      GUI.Box(new Rect(this.buttons_x_corner,10,150,90), "Cameras");
       if(GUI.Button(new Rect(this.buttons_x_corner+10,40,40,40), GunCamera2d))
         {
             if(this.gun_camera_is_hidden){
@@ -60,7 +63,18 @@ public class cameraButtons : MonoBehaviour
                 this.radar_camera_is_hidden = true;
                 this.radar_camera.SetActive(false);
             }
-        }	
+        }
+
+        if(GUI.Button(new Rect(this.buttons_x_corner+105,40,40,40), RearViewIcon)) 
+        {
+            if(this.rearview_camera_is_hidden){
+                this.rearview_camera_is_hidden = false;
+                this.rearview_camera.SetActive(true);
+            } else {
+                this.rearview_camera_is_hidden = true;
+                this.rearview_camera.SetActive(false);
+            }
+        }		
   	}
 
     // Update is called once per frame
