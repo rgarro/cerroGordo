d8b77588f216b9fa7c92cde7f73d86ce70201982
rgarro <rgarro@gmail.com>
2020-05-06 15:05:46 -0600

    Alles klar, Herr Kommissar?

--- Assets/CerroGordo/Scripts/cameraButtons.cs
+++ Assets/CerroGordo/Scripts/cameraButtons.cs
@@ -28,6 +28,7 @@ public class cameraButtons : MonoBehaviour
     public Texture2D GunCamera2d;
     public Texture2D RadarIcon;
     public Texture2D RearViewIcon;
+    public Texture2D MainCameraIcon;
     public GameObject gun_camera;
     public GameObject radar_camera;
     public GameObject main_camera;
@@ -42,7 +43,7 @@ public class cameraButtons : MonoBehaviour
 
     void OnGUI(){
       GUI.skin = this.btnSkin;
-      GUI.Box(new Rect(this.buttons_x_corner,10,150,90), "Cameras");
+      GUI.Box(new Rect(this.buttons_x_corner,10,200,90), "Cameras");
       if(GUI.Button(new Rect(this.buttons_x_corner+10,40,40,40), GunCamera2d))
         {
             if(this.gun_camera_is_hidden){
@@ -73,6 +74,17 @@ public class cameraButtons : MonoBehaviour
             } else {
                 this.rearview_camera_is_hidden = true;
                 this.rearview_camera.SetActive(false);
+            }
+        }
+
+        if(GUI.Button(new Rect(this.buttons_x_corner+155,40,40,40), MainCameraIcon)) 
+        {
+            if(this.main_camera_is_hidden){
+                this.main_camera_is_hidden = false;
+                this.main_camera.SetActive(true);
+            } else {
+                this.main_camera_is_hidden = true;
+                this.main_camera.SetActive(false);
             }
         }		
   	}
