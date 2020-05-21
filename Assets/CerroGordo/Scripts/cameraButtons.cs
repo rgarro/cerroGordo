e407f74a66e12a582efa699f8dfaecdb049ee662
rgarro <rgarro@gmail.com>
2020-05-20 19:05:38 -0600

    take on me take me on ..

--- Assets/CerroGordo/Scripts/cameraButtons.cs
+++ Assets/CerroGordo/Scripts/cameraButtons.cs
@@ -22,6 +22,7 @@ public class cameraButtons : MonoBehaviour
     protected bool radar_camera_is_hidden =  true;
     protected bool rearview_camera_is_hidden = true;
     protected bool main_camera_is_hidden = false;
+    protected bool sound_is_on = true;
     protected int buttons_x_corner = 200;
 
     public GUISkin btnSkin;
@@ -29,13 +30,17 @@ public class cameraButtons : MonoBehaviour
     public Texture2D RadarIcon;
     public Texture2D RearViewIcon;
     public Texture2D MainCameraIcon;
+    public Texture2D SoundIcon;
     public GameObject gun_camera;
     public GameObject radar_camera;
     public GameObject main_camera;
     public GameObject rearview_camera;
+    public AudioSource audioData;
 
     void Start()
     {
+        this.audioData = GetComponent<AudioSource>();
+        this.audioData.Play(0);
         this.gun_camera.SetActive(false);
         this.radar_camera.SetActive(false);
         this.rearview_camera.SetActive(false);
@@ -43,7 +48,7 @@ public class cameraButtons : MonoBehaviour
 
     void OnGUI(){
       GUI.skin = this.btnSkin;
-      GUI.Box(new Rect(this.buttons_x_corner,10,200,90), "Cameras");
+      GUI.Box(new Rect(this.buttons_x_corner,10,265,90), "Cameras");
       if(GUI.Button(new Rect(this.buttons_x_corner+10,40,40,40), GunCamera2d))
         {
             if(this.gun_camera_is_hidden){
@@ -122,12 +127,24 @@ public class cameraButtons : MonoBehaviour
 
 
             
+        }
+
+        if(GUI.Button(new Rect(this.buttons_x_corner+200,40,40,40), SoundIcon)) 
+        {
+               //Debug.Log("sound click ...");
+               if(this.sound_is_on){
+                this.audioData.Pause();
+                this.sound_is_on = false;
+            } else {
+                this.audioData.UnPause();
+                this.sound_is_on = true;
+            }
         }		
   	}
 
     // Update is called once per frame
     void Update()
     {
-
+            
     }
 }
--- README.md
+++ README.md
@@ -3,7 +3,10 @@ cerroGordo
 
 A Drivable Turret Shooter on a Unity3D Vehicle Controller ...
 
+Drive At:
 https://emptyart.github.io/cerroGordo
 
+elCapi Music Loop:
+https://soundcloud.com/dorje-trolo/elcapi
 
 [![que no se resistieran, por que sino los mataban ... ](https://raw.githubusercontent.com/rgarro/cerroGordo/master/cerroshot.png)]
