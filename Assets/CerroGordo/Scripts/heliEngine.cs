ea69bd87c1a646201d473ebd3c906dd9957b619f
rgarro <rgarro@gmail.com>
2020-08-19 17:25:30 -0600

    mi nombre es yasuri yasuri yamile ...

--- Assets/CerroGordo/Scripts/heliEngine.cs
+++ Assets/CerroGordo/Scripts/heliEngine.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class heliEngine : MonoBehaviour {
 
 	public float rotationSteps;
+	public bool is_z = false;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		float rotate = transform.rotation.y + rotationSteps;
-		transform.Rotate(transform.rotation.x,rotate,transform.rotation.x);
+		if(this.is_z){
+			float rotate = transform.rotation.z + rotationSteps;
+			transform.Rotate(0,0,rotate);
+		} else {
+			float rotate = transform.rotation.y + rotationSteps;
+			transform.Rotate(transform.rotation.x,rotate,transform.rotation.x);
+		}
 	}
 }
