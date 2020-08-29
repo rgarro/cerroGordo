d70f34e927beb29cdf0ebfc531b1cbd3453cba4b
rgarro <rgarro@gmail.com>
2020-08-28 20:05:06 -0600

    in a rub a dub style in a rub a dub style ..

--- Assets/CerroGordo/Scripts/destroyByBtrGunHit.cs
+++ Assets/CerroGordo/Scripts/destroyByBtrGunHit.cs
@@ -17,9 +17,9 @@ using UnityEngine;
 public class destroyByBtrGunHit : MonoBehaviour {
 
 	public GameObject Blast;
-	public int scoreValue;
-
+	public int scoreValue = 10;
 	public float stallingTime = 0.8f;
+
 	void OnTriggerEnter(Collider other) {
 		//Debug.Log ("tome chichiii...");
 		if (other.tag == "Boundary") {
@@ -29,8 +29,24 @@ public class destroyByBtrGunHit : MonoBehaviour {
 		//Debug.Log (bt);
 		batiComputadora mu = bt.GetComponent<batiComputadora> ();
 		mu.addScore(scoreValue);
-		Instantiate (Blast,this.transform.position,this.transform.rotation);
+		//Instantiate (Blast,this.transform.position,this.transform.rotation);
+		this.showBlast();
 		Destroy(other.gameObject);
+		//Destroy(gameObject);
+		this.startFalling();
+		Invoke("afterStall",this.stallingTime);
+	}
+
+	private void startFalling(){
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.useGravity = true;
+	}
+	private void afterStall(){
+		this.showBlast();
 		Destroy(gameObject);
+	}
+
+	public void showBlast(){
+		Instantiate (Blast,this.transform.position,this.transform.rotation);
 	}
 }
