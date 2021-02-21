24df51868310f46d13be0080eebb809b5ad06e91
rolando <rgarro@gmail.com>
2021-02-21 15:55:20 -0600

    la ceiba del puente negro canta la verdad ...

---
+++ Assets/CerroGordo/Scripts/autoRestart.cs
@@ -0,0 +1,43 @@
+﻿/**
+ *      _..--""````""--.._
+ *    .'       |\/|       '.
+ *   /    /`._ |  | _.'\    \
+ *  ;    /              \    |
+ *  |   /                \   |
+ *  ;  / .-.          .-. \  ;
+ *   \ |/   \.-.  .-./   \| /
+ *    '._       \/       _.'
+ *       ''--..____..--''
+ *    === DRINK A BEER AT MOES BAR TIBAS ===
+ *   No Vote nunca por Nelson aunque sea  de tibas  , jA Jaa ...
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoRestart : MonoBehaviour
+{
+
+    public GameObject vehicle;
+    public float minPosY;
+
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void doRestart(){
+		//Debug.Log("will restart");
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
