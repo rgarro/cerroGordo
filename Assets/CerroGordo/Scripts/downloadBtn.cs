46b5c7e21bcc8f50e8b00f82a48f90d10f715b84
rolando <rgarro@gmail.com>
2021-06-16 19:28:32 -0600

    natty dread rides again ..

---
+++ Assets/CerroGordo/Scripts/downloadBtn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *
+ *                 _xAXAx_        /|  Ride Natty Ride !!                         ^^
+ *              _xAXXXXXXX=x.    / |  /
+ *          _xAXXXXXXXXV        /x |_o/    ^^                         ^^
+ *      _xAXXXXXXXXXXXA        /---| :
+ *sjo XXXXXXXXXXXXXXXXXAx      '---_/_\_
+ *XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ *
+ * Jah say this. judgement it could never be with water
+ * https://matterporttest-f4387.firebaseapp.com/TheGospelOfLuke.zip
+ * Fire is burning may pull your own weight ...
+ *
+ *@author Rolando <rgarro@gmail.com>
+ */
+public class downloadBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //https://matterporttest-f4387.firebaseapp.com/TheGospelOfLuke.zip
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
